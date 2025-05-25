d0d8d8fdbde12c892eeae98de0d34a81c06d8f28
Julian Cissen <107254512+JulianCissen@users.noreply.github.com>
2025-05-25 21:51:46 +0200

    feat: add Jest configuration and update package.json for testing support chore: update .gitignore to include coverage directory

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+export default {
+    preset: 'ts-jest/presets/default-esm',
+    testEnvironment: 'node',
+    extensionsToTreatAsEsm: ['.ts'],
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    transform: {
+        '^.+\\.ts$': [
+            'ts-jest',
+            {
+                useESM: true,
+            },
+        ],
+    },
+    transformIgnorePatterns: ['node_modules/(?!(jose|openid-client)/)'],
+    testMatch: ['**/__tests__/**/*.test.ts'],
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts', '!src/**/index.ts'],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov', 'html'],
+};
